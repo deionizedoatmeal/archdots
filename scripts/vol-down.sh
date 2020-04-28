5f3ab7210bbe5386bb3fab9f464eceecbf4e7670
deionizedoatmeal <ian.k.bania@gmail.com>
2020-04-28 04:05:24 -0700

    vol scripts now adjust volume for sinks 0-10

--- scripts/vol-down.sh
+++ scripts/vol-down.sh
@@ -3,6 +3,13 @@
 (pactl set-sink-volume 1 -5%)
 (pactl set-sink-volume 2 -5%)
 (pactl set-sink-volume 3 -5%)
+(pactl set-sink-volume 4 -5%)
+(pactl set-sink-volume 5 -5%)
+(pactl set-sink-volume 6 -5%)
+(pactl set-sink-volume 7 -5%)
+(pactl set-sink-volume 8 -5%)
+(pactl set-sink-volume 9 -5%)
+(pactl set-sink-volume 10 -5%)
 
 VOL=$(pacmd dump-volumes | awk 'NR==1{print $8}' | sed 's/\%//')
 notify-send 墳 $VOL -h string:x-canonical-private-synchronous:anything 
--- scripts/vol-up.sh
+++ scripts/vol-up.sh
@@ -6,6 +6,13 @@ if [[ $VOL -lt 100 ]]; then
         (pactl set-sink-volume 1 +5%)
         (pactl set-sink-volume 2 +5%)
         (pactl set-sink-volume 3 +5%)
+        (pactl set-sink-volume 4 +5%)
+        (pactl set-sink-volume 5 +5%)
+        (pactl set-sink-volume 6 +5%)
+        (pactl set-sink-volume 7 +5%)
+        (pactl set-sink-volume 8 +5%)
+        (pactl set-sink-volume 9 +5%)
+        (pactl set-sink-volume 10 +5%)
 fi
 VOL=$(pacmd dump-volumes | awk 'NR==1{print $8}' | sed 's/\%//')
 notify-send 墳 $VOL -h string:x-canonical-private-synchronous:anything
