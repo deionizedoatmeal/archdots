f31e79436656654bafa51356cc3e6a498ad6052a
deionizedoatmeal <ian.k.bania@gmail.com>
2020-04-28 11:42:57 -0700

    standardizing poly bar across neptune and triton

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -2,12 +2,13 @@
 
 (wal -i "$(< "${HOME}/.cache/wal/wal")")
 
-# Terminate already running bar instances
+# terminate already running bar instances
 killall -q polybar
 
-# Wait until the processes have been shut down
+# wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar main
+# launch bars
+polybar main &
+polybar secondary &
 
