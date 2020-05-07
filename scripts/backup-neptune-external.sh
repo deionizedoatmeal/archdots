fcbc6c96fceb2b2d6fcd4118bdeb5815c757c219
deionizedoatmeal <ian.k.bania@gmail.com>
2020-05-07 13:22:06 -0700

    added a file copy before running rdiff

--- scripts/backup-neptune-external.sh
+++ scripts/backup-neptune-external.sh
@@ -5,5 +5,8 @@ lsblk
 echo \ 
 read -r -p "IS EVERYTHING MOUNTED THAT NEEDS TO BE??? [y/N]" response
 if [[ "$response" =~ ^([Yy])+$ ]]; then
-    sudo rdiff-backup -v5 --exclude '/home/ian/.*' /home/ian/ /mnt/external/
+        # copy my civ saves over lol
+        cp -r .local/share/aspyr-media/Sid\ Meier\'s\ Civilization\ VI/Saves Steam/saves    
+        # backup system
+        sudo rdiff-backup -v5 --exclude '/home/ian/.*' /home/ian/ /mnt/external/
 fi
