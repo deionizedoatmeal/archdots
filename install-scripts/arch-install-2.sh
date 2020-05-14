2682e94e867e8d2fd0dfbca9311b1062fd9270cd
deionizedoatmeal <ian.k.bania@gmail.com>
2020-05-13 19:31:48 -0700

    got hostname to insert in the right file

--- install-scripts/arch-install-2.sh
+++ install-scripts/arch-install-2.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
 # arch install part 2 electric buggalo
 
+#read in variables
+DISK=$(cat /archdots/system/disk)
+DISKP=$(cat /archdots/system/diskp)
+
 # aic94xx-firmware
-cd aic94xx-firmware
-makepkg -sri
-cd ..
+#cd aic94xx-firmware
+#makepkg -sri
+#cd ..
 rm -r aic94xx-firmware
 
 # wd719x-firmware
-cd wd719x-firmware
-makepkg -sri
-cd ..
+#cd wd719x-firmware
+#makepkg -sri
+#cd ..
 rm -r wd719x-firmware
 
 echo "Firmware installed."
-
 # user confirmation of lvm and luks setuo
 echo "Disk should be partioned and volumized as such:"
 echo "#########################################################"
 echo "${DISK}"
-echo "--> ${DISKP}1 -------- 550M ------------ part ----- /efi"
-echo "--> ${DISKP}2 -------- rest of disk ---- part ----- "
+echo "--> ${DISKP}1 -------- 250M ------------ part ----- /boot"
+echo "--> ${DISKP}2 -------- 550M ------------ part ----- /boot/efi"
+echo "--> ${DISKP}3 -------- rest of disk ---- part ----- "
 echo "   --> cryptlvm ------ rest of disk ---- crypt ---- "
 echo "      --> vg-swap ---- 8G -------------- lvm ------ [SWAP]"
 echo "      --> vg-root ---- 20G ------------- lvm ------ /"
@@ -38,7 +42,7 @@ fi
 
 while true; do
         # set timezone
-        read -r -p "Pick a timezone, some options are: America/Anchorage, America/Los_Angeles, America/Denver, America/Chicago, America/New_York, America/Santiago, America/Sao_Paulo, Europe/London, Europe/Berlin, Europe/Istanbul, Europe/Moscow, Asia/Honk_Kong, Asia/Tokyo, Australia/Canberra, Pacific/Honolulu" TIMEZN
+        read -r -p "Pick a timezone, some options are: America/Anchorage, America/Los_Angeles, America/Denver, America/Chicago, America/New_York, America/Santiago, America/Sao_Paulo, Europe/London, Europe/Berlin, Europe/Istanbul, Europe/Moscow, Asia/Honk_Kong, Asia/Tokyo, Australia/Canberra, Pacific/Honolulu $'\n'Timezone: " TIMEZN
 
         if [[ -f /usr/share/zoneinfo/${TIMEZN} ]]; then
                 ln -sf /usr/share/zoneinfo/${TIMEZN} /etc/localtime
@@ -54,30 +58,33 @@ hwclock --systohc
 
 # set hostname
 read -r -p "Chose a hostname: " HNAME
-touch /etc/hostname && cat /etc/hostname ${HNAME}
+touch /etc/hostname
+echo ${HNAME} >> /etc/hostname
 
 # set up /etc/hosts
-echo "127.0.1.1 	${HNAME}.localdomain	${HNAME}" >> /dots/system/hosts_incomplete
-cp /dots/system/hosts_incomplete /etc/hosts
+echo "127.0.1.1 	${HNAME}.localdomain	${HNAME}" >> /archdots/system/hosts_incomplete
+cp /archdots/system/hosts_incomplete /etc/hosts
 
 
 # get the UUID for LUKS partition
-LUKSUUID=$(blkid | grep "${DISKP}2" | grep -o "UUID=.*" | cut -d\" -f2)
+LUKSUUID=$(blkid | grep "${DISKP}3" | grep -o "UUID=.*" | cut -d\" -f2)
 
 # insert that into tempelate for /etc/default/grub
-LINEINSERT$="GRUB_CMDLINE_LINUX="cryptdevice=UUID=${LUKSUUID}:cryptlvm root=/dev/vg/root cryptkey=rootfs:/root/secrets/crypto_keyfile.bin""
+LINEINSERT=$(echo "10iGRUB_CMDLINE_LINUX="cryptdevice=UUID=${LUKSUUID}:cryptlvm root=/dev/vg/root cryptkey=rootfs:/root/secrets/crypto_keyfile.bin"")
 
-awk '/INSERT_UUID_OF_LUKS_PARTITION_HERE/ { print; print $LINEINSERT; next }1' /dots/system/grub
+sed -i "${LINEINSERT}" /archdots/system/grub
 
-# copy /etc/sudoers /etc/mkinitcpio.conf and /etc/default/grub over
-cp -p /dots/system/sudoers /etc/sudoers
-cp -p /dots/system/mkinitcpio.conf /etc/mkinitcpio.conf
-cp -p /dots/system/grub /etc/default/grub
+# copy /etc/default/grub over
+cp -p /archdots/system/grub /etc/default/grub
+
+# copy /etc/sudoers and /etc/mkinitcpio.conf over
+cp -p /archdots/system/sudoers /etc/sudoers
+cp -p /archdots/system/mkinitcpio.conf /etc/mkinitcpio.conf
 
 
 # install grub
 pacman -S efibootmgr grub
-grub-install --target=x86_64-efi --efi-directory=/efi
+grub-install --target=x86_64-efi --efi-directory=/boot/efi
 
 # intel microcode updates
 pacman -S intel-ucode
@@ -85,7 +92,8 @@ pacman -S intel-ucode
 # create a keyfile to embed in initramfs
 mkdir /root/secrets && chmod 700 /root/secrets
 head -c 64 /dev/urandom > /root/secrets/crypto_keyfile.bin && chmod 600 /root/secrets/crypto_keyfile.bin
-cryptsetup -v luksAddKey -i 1 /dev/${DISKP}2 /root/secrets/crypto_keyfile.bin
+echo "Now enter your disk encryption password!"
+cryptsetup -v luksAddKey -i 1 /dev/${DISKP}3 /root/secrets/crypto_keyfile.bin
 
 # create initramfs image
 mkinitcpio -p linux
@@ -127,12 +135,21 @@ while true; do
         fi
 done
 
-# reclone dots for ease of accses after reboot 
+# install graphics drivers
+read -r -p "Do you want to install nvidia drivers? [Y/n]" response
+if [[ "$response" =~ ^([Nn])+$ ]]; then
+        echo "cool"
+else
+        pacman -S nvidia
+fi
+
+
+# reclone archdots for ease of accses after reboot 
 cd /home/${UNAME}
 mkdir Repos && cd Repos
-git clone https://github.com/deionizedoatmeal/dots.git
+git clone https://github.com/deionizedoatmeal/archdots.git
 
 # remove this repo
-rm -r /dots
+rm -r /archdots
 
 echo "Now check everything, exit and reboot"
