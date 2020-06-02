8c9d60946a8ede9bf88361a891b24da171dd6ad7
deionizedoatmeal <ian.k.bania@gmail.com>
2020-06-01 23:43:09 -0700

    package list update

--- install-scripts/software-install.sh
+++ install-scripts/software-install.sh
@@ -16,7 +16,7 @@ NONES="pavucontrol libreoffice-fresh cheese nmap htop ranger zathura zathura-pdf
 
 TRI="tlp tlp-rdw powertop upower"
 
-NEP="virtualbox nvidia"
+NEP="virtualbox virtualbox-host-modules-arch nvidia"
 
 #python3-venv?
 #pipx?
