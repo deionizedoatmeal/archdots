f4afdc9fea194d3cf3d2ce0b2e63b16f5e27175a
deionizedoatmeal <ian.k.bania@gmail.com>
2020-06-06 00:02:54 -0700

    fixed the pass setup script

--- install-scripts/pass-setup.sh
+++ install-scripts/pass-setup.sh
@@ -2,7 +2,17 @@
 
 pass init ian.k.bania@gmail.com
 pass git init
-pass git remote add origin ian@neptune:/home/ian/Repos/pass
+read -r -p "Remote [R] or local [L] ?" RESPONSE
+
+if [[ "$RESPONSE" =~ ^([Rr])+$ ]]; then
+        pass git remote add origin ian@neptune:/home/ian/Repos/pass
+elif [[ "$response" =~ ^([Ll])+$ ]]; then
+        pass git remote add origin /home/ian/Repos/pass
+else
+        echo "Invalid reponse."
+        set -e 
+fi
+
 pass git pull origin master --allow-unrelated-histories
 
 cp ../system/gpg-agent.conf ${HOME}/.gnupg/.
