6b6477f69184b565c4ac9c8347d88fa36a77f568
deionizedoatmeal <ian.k.bania@gmail.com>
2020-06-23 13:34:40 -0700

    backup script

--- scripts/backup-neptune-external.sh
+++ scripts/backup-neptune-external.sh
@@ -6,7 +6,7 @@ echo \
 read -r -p "IS EVERYTHING MOUNTED THAT NEEDS TO BE??? [y/N]" response
 if [[ "$response" =~ ^([Yy])+$ ]]; then
         # copy my civ saves over lol
-        cp -r .local/share/aspyr-media/Sid\ Meier\'s\ Civilization\ VI/Saves Steam/saves    
+        cp -r ${HOME}/.local/share/aspyr-media/Sid\ Meier\'s\ Civilization\ VI/Saves Steam/saves    
         # backup system
         sudo rdiff-backup -v5 --exclude '/home/ian/.*' /home/ian/ /mnt/external/
 fi
