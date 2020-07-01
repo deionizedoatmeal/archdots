9645ceb12ef6d6f87e7b5bd69e33d328d3120edb
deionizedoatmeal <ian.k.bania@gmail.com>
2020-06-30 22:13:55 -0700

    added some nice icons and strength indicator

--- scripts/rofimenu-wifi.sh
+++ scripts/rofimenu-wifi.sh
@@ -18,13 +18,12 @@ elif [[ "$CONSTATE" =~ "disabled" ]]; then
 fi
 
 # make an empty rofi menu while scaning for networks
-echo "scaning..." | rofi -dmenu -p "wifi $ONOFF" -lines 1 -width 24 &
+echo "scaning networks..." | rofi -dmenu -p "wifi $ONOFF" -lines 1 -width 23 &
 
-# SSID,SECURITY could also be used here
-FIELDS=SSID
+# SSID,SECURITY,BARS,SIGNAL could also be used here
+FIELDS=SSID,BARS
 
-LIST=$(nmcli --fields "$FIELDS" device wifi list | sed '/^--/d' | sed '/^SSID/d' | sed '/^181/d' | uniq -u)
-echo $LIST
+LIST=$(nmcli --fields "$FIELDS" device wifi list | sed '/^--/d' | sed '/^SSID/d' | sed '/^181/d' | uniq -u | sed 's/^/直 /')
 
 # dynamically change the height of the rofi menu
 LINENUM=$(echo "$LIST" | wc -l)
@@ -35,10 +34,6 @@ KNOWNCON=$(nmcli connection show)
 
 # find the current name of our network
 CURRSSID=$(LANGUAGE=C nmcli -t -f active,ssid dev wifi | awk -F: '$1 ~ /^yes/ {print $2}')
-
-#if [[ ! -z $CURRSSID ]]; then
-#	HIGHLINE=$(echo  "$(echo "$LIST" | awk -F "[  ]{2,}" '{print $1}' | grep -Fxn -m 1 "$CURRSSID" | awk -F ":" '{print $1}') + 1" | bc )
-#fi
 
 # if there are more than 14 SSIDs, the menu will still only have 14 lines
 if [ "$LINENUM" -gt 14 ] && [[ "$CONSTATE" =~ "enabled" ]]; then
@@ -51,11 +46,15 @@ fi
 killall rofi
 
 # start the real rofi menu
-CHENTRY=$(echo -e "$TOGGLE\nmanual\n$LIST" | rofi -dmenu -p "wifi $ONOFF" -lines "$LINENUM" -width 24)
-CHSSID=$(echo "$CHENTRY" | sed  's/\s\{2,\}/\|/g' | awk -F "|" '{print $1}')
+CHENTRY=$(echo -e "$TOGGLE\n manual\n$LIST" | rofi -dmenu -p "wifi $ONOFF" -lines "$LINENUM" -width 23)
+CHSSID=$(echo "$CHENTRY" | sed  's/\s\{2,\}/\|/g' | awk -F "|" '{print $1}' | cut -c 5-)
+
+# exit if the user didn't select anything
+if [ "$CHENTRY" = "" ] ; then
+        exit 0
 
 # if the user inputs "manual" as their SSID in the start window, it will bring them to this screen
-if [ "$CHENTRY" = "manual" ] ; then
+elif [ "$CHENTRY" = " manual" ] ; then
 	# Manual entry of the SSID and password (if appplicable)
 	MSSID=$(echo "enter the SSID of the network (SSID,password)" | rofi -dmenu -p "Manual Entry: " -lines 1)
 	# Separating the password from the entered string
@@ -79,15 +78,17 @@ else
 	# If the connection is already in use, then this will still be able to get the SSID
 	if [ "$CHSSID" = "*" ]; then
 		CHSSID=$(echo "$CHENTRY" | sed  's/\s\{2,\}/\|/g' | awk -F "|" '{print $3}')
+        
+        # remove the icon character from the front of the string and the space from the end
+        CHSSID=${CHSSID:4}
+        CHSSID=${CHSSID:-1}
 	fi
 
-	# Parses the list of preconfigured connections to see if it already contains the chosen SSID. This speeds up the connection process
+	# parses the list of preconfigured connections to see if it already contains the chosen SSID. This speeds up the connection process
 	if [[ $(echo "$KNOWNCON" | grep "$CHSSID") = "$CHSSID" ]]; then
-		nmcli con up "$CHSSID"
+        nmcli con up "$CHSSID"
 	else
-		if [[ "$CHENTRY" =~ "WPA2" ]] || [[ "$CHENTRY" =~ "WEP" ]]; then
-			WIFIPASS=$(echo "if connection is stored, hit enter" | rofi -dmenu -p "password: " -lines 1 )
-		fi
+		WIFIPASS=$(rofi -dmenu -p "password: " -lines 0 )
 		nmcli dev wifi con "$CHSSID" password "$WIFIPASS"
 	fi
 
