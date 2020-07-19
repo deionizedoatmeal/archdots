ccde61a51e13c8f2d14445c7ed97954e13c3cd29
deionizedoatmeal <ian.k.bania@gmail.com>
2020-07-19 14:15:19 -0700

    fixing exit menu

--- scripts/rofimenu-exit.sh
+++ scripts/rofimenu-exit.sh
@@ -13,10 +13,10 @@ options="$power_off\n$reboot\n$lock\n$log_out"
 chosen="$(echo -e "$options" | $rofi_command -dmenu -p "session" -selected-row 2)"
 case $chosen in
     $power_off)
-        systemctl poweroff
+        sudo systemctl poweroff
         ;;
     $reboot)
-        systemctl reboot
+        sudo systemctl reboot
         ;;
     $lock)
         gllock
