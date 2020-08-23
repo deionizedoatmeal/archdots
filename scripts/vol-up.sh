d63f57dd52c2e8e8e935d79719a15a55f1f29887
deionizedoatmeal <ian.k.bania@gmail.com>
2020-08-23 16:10:07 -0700

    vol scripts apply to all active sinks

--- scripts/vol-up.sh
+++ scripts/vol-up.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
 VOL=$(pacmd dump-volumes | awk 'NR==1{print $8}' | sed 's/\%//')
+SINKS=$(pacmd dump-volumes | grep -Eo "Sink.{0,3}" | cut -b 6-7)
 
 if [[ $VOL -lt 100 ]]; then
-        (pactl set-sink-volume 0 +5%)
-        (pactl set-sink-volume 1 +5%)
-        (pactl set-sink-volume 2 +5%)
-        (pactl set-sink-volume 3 +5%)
-        (pactl set-sink-volume 4 +5%)
-        (pactl set-sink-volume 5 +5%)
-        (pactl set-sink-volume 6 +5%)
-        (pactl set-sink-volume 7 +5%)
-        (pactl set-sink-volume 8 +5%)
-        (pactl set-sink-volume 9 +5%)
-        (pactl set-sink-volume 10 +5%)
+    for I in $SINKS; do
+        (pactl set-sink-volume $I +5%)
+    done
+        
+#    (pactl set-sink-volume 0 +5%)
+#    (pactl set-sink-volume 1 +5%)
+#    (pactl set-sink-volume 2 +5%)
+#    (pactl set-sink-volume 3 +5%)
+#    (pactl set-sink-volume 4 +5%)
+#    (pactl set-sink-volume 5 +5%)
+#    (pactl set-sink-volume 6 +5%)
+#    (pactl set-sink-volume 7 +5%)
+#    (pactl set-sink-volume 8 +5%)
+#    (pactl set-sink-volume 9 +5%)
+#    (pactl set-sink-volume 10 +5%)
+
 fi
+
 VOL=$(pacmd dump-volumes | awk 'NR==1{print $8}' | sed 's/\%//')
+
 notify-send "墳 $VOL" -h string:x-canonical-private-synchronous:anything
