9ee6b1755aba2277e0e16e3d4290ecb6033bb34a
deionizedoatmeal <ian.k.bania@gmail.com>
2020-08-26 14:22:32 -0700

    package list update

--- install-scripts/software-install.sh
+++ install-scripts/software-install.sh
@@ -71,7 +71,7 @@ YAYSYS="fet.sh-git brother-mfc-9560cdw ttf-symbola paper-icon-theme vim-plug gll
 #python-pipx teamviewer autotiling
 YAYNEPTUNE="siril-git google-earth-pro virtualbox-ext-oracle remmina-plugin-rdesktop"
 YAYTRITON="system76-power system76-firmware-daemon system76-driver light-git"
-YAYNONES="slack-desktop slack-term-bin spotify mfetch-git android-messages-desktop zoom"
+YAYNONES="slack-desktop slack-term-bin spotify-tui-git spotify mfetch-git android-messages-desktop zoom"
 ##########################################################################
 # yay install
 
@@ -173,7 +173,8 @@ fi
 # install rust packages    
 read -r -p "Would you like to install rust packages? [y/N]" response 
 if [[ "$response" =~ ^([Yy])+$ ]]; then
-        cargo install spotify-tui
+        #cargo install spotify-tui
+        #using AUR now
         cargo install bottom
         cargo install dijo
         cargo install -f --git https://github.com/cjbassi/ytop ytop
