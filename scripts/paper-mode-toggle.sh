63b5b3ff080ebc5651e5752f6c6a5c94c163e5d7
deionizedoatmeal <ian.k.bania@gmail.com>
2020-08-29 18:57:04 -0700

    tuning the paper mode script to work with my weird monitor setup

--- scripts/paper-mode-toggle.sh
+++ scripts/paper-mode-toggle.sh
@@ -2,35 +2,50 @@
 # by ian k. bania 
 # feb '20
 
-
-#grepstuff=$(i3-msg -t get_tree | grep -oP 'gaps":{"inner":0,"outer":0,"top":0,"right":.{0,3}' | tail -1 | grep -o '.\{3\}$')
-#grepstuff=$()
-#grepstuff=$(i3-msg -t get_tree | grep -oP '750' | tail -1)
-#grepstuff=$(i3-msg -t get_tree | grep -oP '.{0,250}"focused":true' | grep -oP 'right.{0,5}' | grep -o '.\{3\}$')
-#GREPSTUFF=$(i3-msg -t get_tree | grep -oP '.{0,1500}"focused":true' | grep -oP 'gaps.{0,65}' | grep -oP '"right":.{0,3}' | cut -c 9-9)
-
-
+# get the info from i3wm
 I3MSG=$(i3-msg -t get_tree)
+
+# check to see what dispaly its on
 DELIM='"focused":true'
-#echo ${DELIM}
+LONGDISP=${I3MSG##*${DELIM}}
+DELIM=',"layout":'
+DISP=${LONGDISP%%${DELIM}*}
+DELIM='"output":'
+WORD=${DISP##*${DELIM}}
+DELIM='"'
+WORD=${WORD#*${DELIM}}
+DELIM='"'
+DISPLAY=${WORD%${DELIM}*}
+DELIM='-'
+HDMI=${DISPLAY%%${DELIM}*}
+
+# check to see what the right gaps are set to
+DELIM='"focused":true'
 FOCUSED=${I3MSG%%${DELIM}*}
-#echo ${FOCUSED}
 GAPS=${FOCUSED##*gaps}
-#echo ${GAPS}
 DELIM='"right":'
-#echo ${DELIM}
 RIGHT=${GAPS##*${DELIM}}
-#echo ${RIGHT}
 RIGHTVALUE=${RIGHT%%,*}
-#echo "-!--> *** ***"
-#echo ${RIGHTVALUE}
+
 
 if [[ ${RIGHTVALUE} -eq 0 ]]; then
-    i3-msg gaps right current set $1;
-    i3-msg gaps left current set $1;
-    i3-msg gaps top current set $2;
-    i3-msg gaps bottom current set $3;
-#    i3-msg split v;
+    if [[ $HDMI == "HDMI" ]]; then
+        i3-msg gaps right current set 100;
+        i3-msg gaps left current set 100;
+        i3-msg gaps top current set 300;
+        i3-msg gaps bottom current set 300;
+    elif [[ ${DISPLAY} == "DisplayPort-2" ]]; then
+        i3-msg gaps right current set 200;
+        i3-msg gaps left current set 200;
+        i3-msg gaps top current set 100;
+        i3-msg gaps bottom current set 100;
+    else
+        i3-msg gaps right current set $1;
+        i3-msg gaps left current set $1;
+        i3-msg gaps top current set $2;
+        i3-msg gaps bottom current set $3;
+#        i3-msg split v;
+    fi
 else
     i3-msg gaps right current set 0;
     i3-msg gaps left current set 0;
