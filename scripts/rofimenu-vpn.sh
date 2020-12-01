aff88900ab005b711ca4bbb0ec8cc66b8d958a93
deionizedoatmeal <ian.k.bania@gmail.com>
2020-12-01 13:26:43 -0800

    basic vpn rofi menu

--- scripts/rofimenu-vpn.sh
+++ scripts/rofimenu-vpn.sh
@@ -2,40 +2,40 @@
 
 # check for triton (hidpi) flag
 if [[ "$1" =~ ^([Tt])+$ ]]; then
-        rofi_command="rofi -lines 3 -width 450"
+        rofi_command="rofi -lines 2 -width 500"
 else
-        rofi_command="rofi -lines 3 -width 250"
+        rofi_command="rofi -lines 2 -width 300"
 fi
 
 ### Options ###
-MSG=$(protonvpn s)
-STATUS=$(echo $MSG | grep -o '^...........')
-IP=$(echo $MSG | awk '/IP:/ {match($0, /IP:/); print substr($0, RSTART + 3, RLENGTH + 11);}')
+# MSG=$(protonvpn s)
+# STATUS=$(echo $MSG | grep -o '^...........')
+STATUS=$(ps -ax | grep openconnect | wc -l)
+# IP=$(echo $MSG | awk '/IP:/ {match($0, /IP:/); print substr($0, RSTART + 3, RLENGTH + 11);}')
 
-if [ "$STATUS" == "Status: Con" ]
+# if [ "$STATUS" == "Status: Con" ]
+if [ "$STATUS" = "1" ] || [ "$STATUS" = "0" ]
 then
-    zero=" connect vpn <-"
-else
     zero=" connect vpn"
-fi
-
-
-if [ "$STATUS" == "Status: Dis" ]
-then
     first=" disconnect vpn <-"
 else
+    zero=" connect vpn <-"
     first=" disconnect vpn"
 fi
+
 
 # Variable passed to rofi
 options="$zero\n$first"
 
-chosen="$(echo -e "$options" | $rofi_command -dmenu -p "vpn$IP" -selected-row 2)"
+# chosen="$(echo -e "$options" | $rofi_command -dmenu -p "vpn$IP" -selected-row 2)"
+chosen="$(echo -e "$options" | $rofi_command -dmenu -p "vpn" -selected-row 2)"
 case $chosen in
     $zero)
-        sudo protonvpn -f c
+        # sudo protonvpn -f c
+        vpn-connect
         ;;
     $first)
-        sudo protonvpn d
+        # sudo protonvpn d
+        vpn-disconnect
 esac
 
