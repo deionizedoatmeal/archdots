5690c71f70c21d1458a7dd678fa878d778e8e162
deionizedoatmeal <ian.k.bania@gmail.com>
2020-12-05 09:24:15 -0800

    vpn script

---
+++ scripts/vpn-connect.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# sudo openconnect --protocol=gp --user=ibania remoteaccess.colgate.edu
+sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | sudo openconnect --protocol=gp remoteaccess.colgate.edu
+ibania
+***REMOVED***
+SplitTunnel
+EOF
+
