4aad26e32f8d980a66a324710022d911d3d97cb4
deionizedoatmeal <ian.k.bania@gmail.com>
2021-02-04 19:21:45 -0800

    polybar nonsense

---
+++ xprofiles/triton_external.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP1 --mode 3200x1800 --pos 3840x0 --rotate normal --output DP1 --off --output HDMI1 --off --output HDMI2 --scale 2x2 --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
