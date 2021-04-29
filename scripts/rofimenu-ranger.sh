f64a88f5842ed5f473993f0f04652de99d8c7e65
Ian K. Bania <ian.k.bania@gmail.com>
2021-04-29 01:04:06 -0700

    quick launch for commonly used directories

---
+++ scripts/rofimenu-ranger.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# check for triton (hidpi) flag
+if [[ "$1" =~ ^([Tt])+$ ]]; then
+        rofi_command="rofi -lines 5 -width 400"
+else
+        rofi_command="rofi -lines 5 -width 250"
+fi
+
+
+### Options ###
+dl=" downloads"
+rp=" repos"
+sch=" school"
+dm=" dark matter"
+p="﬌  papers"
+# Variable passed to rofi
+options="$dl\n$rp\n$sch\n$dm\n$p"
+
+chosen="$(echo -e "$options" | $rofi_command -dmenu -p "open" -selected-row 1)"
+case $chosen in
+    $dl)
+        kitty ranger ~/Downloads > /dev/null 2>&1
+        ;;
+    $rp)
+        kitty ranger ~/Repos > /dev/null 2>&1
+        ;;
+    $sch)
+        kitty ranger ~/School > /dev/null 2>&1
+        ;;
+    $dm)
+        kitty ranger ~/Research/dark_matter > /dev/null 2>&1
+        ;;
+    $p)
+        kitty ranger ~/Research/dark_matter/papers > /dev/null 2>&1
+        ;;
+esac
+
