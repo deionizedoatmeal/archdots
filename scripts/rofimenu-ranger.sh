badf647c62f8c35c66382f61d9317ba0c99b447b
deionizedoatmeal <ian.k.bania@gmail.com>
2021-04-29 19:58:38 -0700

    added texts to shortcut list

--- scripts/rofimenu-ranger.sh
+++ scripts/rofimenu-ranger.sh
@@ -2,9 +2,9 @@
 
 # check for triton (hidpi) flag
 if [[ "$1" =~ ^([Tt])+$ ]]; then
-        rofi_command="rofi -lines 5 -width 400"
+        rofi_command="rofi -lines 6 -width 400"
 else
-        rofi_command="rofi -lines 5 -width 250"
+        rofi_command="rofi -lines 6 -width 250"
 fi
 
 
@@ -14,8 +14,9 @@ rp=" repos"
 sch=" school"
 dm=" dark matter"
 p="﬌  papers"
+t=" texts"
 # Variable passed to rofi
-options="$dl\n$rp\n$sch\n$dm\n$p"
+options="$dl\n$rp\n$sch\n$dm\n$p\n$t"
 
 chosen="$(echo -e "$options" | $rofi_command -dmenu -p "open" -selected-row 1)"
 case $chosen in
@@ -33,6 +34,9 @@ case $chosen in
         ;;
     $p)
         kitty ranger ~/Research/dark_matter/papers > /dev/null 2>&1
+        ;;
+    $t)
+        kitty ranger ~/Documents/texts > /dev/null 2>&1
         ;;
 esac
 
