e4547856ed8aa6e716570b18c879c5011b7b8bcf
Ian K. Bania <ian.k.bania@gmail.com>
2021-04-30 17:07:42 -0700

    montitor change up

--- xprofiles/neptune.sh
+++ xprofiles/neptune.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DisplayPort-0 --primary --mode 1920x1080 --rate 74.99 --pos 1280x349 --rotate normal --output DisplayPort-1 --mode 1280x1024 --rate 75.02 --pos 0x405 --rotate normal --output DisplayPort-2 --off --output HDMI-A-0 --mode 1920x1080 --pos 3200x0 --rotate right --output DVI-D-0 --off
+xrandr --output DisplayPort-0 --primary --mode 1920x1080 --pos 1080x353 --rotate normal --output DisplayPort-1 --mode 1920x1080 --pos 0x0 --rotate left --output DisplayPort-2 --off --output HDMI-A-0 --mode 1920x1080 --pos 3000x0 --rotate right --output DVI-D-0 --off
