a3403bb319efe39bfbd9e51c2da1e5a9a3b7f99f
deionizedoatmeal <ian.k.bania@gmail.com>
2021-05-31 23:28:46 -0700

    cleaning up

--- scripts/vpn-connect.sh
+++ scripts/vpn-connect.sh
@@ -4,7 +4,7 @@
 {
 sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | sudo openconnect --protocol=gp remoteaccess.colgate.edu
 ibania
-***REMOVED***
+PASSHERE
 SplitTunnel
 EOF
 } </dev/null &>/dev/null &
